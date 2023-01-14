ef60cc7079eb613bbe716b313acfc18485b57f62
JunayedIslam1 <junayedjohny@gmail.com>
2023-01-15 01:49:23 +0600

    All Set Of Curier Service

---
+++ couriermanage/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Courier
+
+
+class CourierAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'student_rollno', 'date_recieved', 'service')
+    change_list_template = 'admin/change_list.html'
+    actions = None
+
+
+admin.site.site_header = 'Maanshan Courier Management System'
+admin.site.site_title = 'Curier Admin'
+admin.site.register(Courier, CourierAdmin)
